6d5f5a4f8e44567dabc11cd76c2c1dddc36eb72e
LuchoLorenzo1 <lucianolorenzo1@hotmail.com>
2022-07-30 21:00:50 -0300

    clear styles when re-running

--- main.js
+++ main.js
@@ -76,3 +76,25 @@ function crearGrilla() {
     grilla.appendChild(tr)
   }
 }
+
+function clearStylesGrilla() {
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      const td = document.getElementById(`${i}:${j}`)
+			let nPeso = pesos[i][j]
+
+			if (nPeso == Infinity) {
+				td.style.background = 'black'
+				// td.style.color = 'white'
+			} else if (nPeso > 1) {
+				td.style.background = `rgb(${200 - 10 * nPeso},${ 200 - 10 * nPeso },${100})`
+			} else {
+				console.log(i,j)
+				td.style.background = 'white'
+			}
+
+    }
+  }
+	document.getElementById(origen).style.background = 'green'
+	document.getElementById(destino).style.background = 'blue'
+}
