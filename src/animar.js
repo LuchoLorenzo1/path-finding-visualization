f7fe28ebe0fd149ade378489a5c1a16af0a80a38
LuchoLorenzo1 <lucianolorenzo1@hotmail.com>
2022-08-08 10:37:31 -0300

    build

--- src/animar.js
+++ src/animar.js
@@ -1,45 +1,28 @@
-import {cambiarCelda} from "./grilla.js"
+import { cambiarCelda } from './grilla.js'
 
 export default function animar(visitados, camino) {
-	let speed = 30;
-	let duration = 50;
-	// let fill = "forwards"
-	let timeout = 0;
+  let speed = 5
+  let timeout = 0
 
+  for (let i = 1; i < visitados.length - 1; i++) {
+    timeout += speed
+    let e = document.getElementById(visitados[i])
 
-	for (let i = 1; i < visitados.length-1; i++) {
-	  timeout += speed;
-		let e = document.getElementById(visitados[i])
+    setTimeout(() => {
+      cambiarCelda(e, 'visitados')
+    }, timeout)
+  }
 
-		setTimeout(()=>{
-			cambiarCelda(e, "visitados")
-		}, timeout)
+  for (let i = 1; i < camino.length; i++) {
+    let e = document.getElementById(camino[i])
+    setTimeout(() => {
+      cambiarCelda(e, 'path')
+    }, timeout)
+    timeout += speed
+  }
 
-		// let a = e.animate([
-		// 	{ background: 'white' },
-		// 	{ background: 'orange' },
-		// ], {
-		// 	duration,
-		// 	delay: timeout,
-		//   iterations: '1',
-		// 	// fill,
-		// });
-		// a.finished.then(() => {
-		// 	cambiarCelda(e, "visitados")
-		// })
-
-	}
-	timeout += 50;
-	for (let i = 1; i < camino.length; i++) {
-		let e = document.getElementById(camino[i])
-		setTimeout(()=>{
-			cambiarCelda(e, "path")
-		}, timeout)
-	  timeout += speed;
-	}
-
-	setTimeout(()=>{
-		window.animating = false
-		window.isClean = false
-	}, timeout)
+  setTimeout(() => {
+    window.animating = false
+    window.isClean = false
+  }, timeout)
 }
